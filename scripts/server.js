cb4bc989d30960928befff2ab5c624222c0ae1b8
Bryan Anderson <banderson5144@gmail.com>
2021-04-26 16:58:35 -0400

    Change from use to get

--- scripts/server.js
+++ scripts/server.js
@@ -18,7 +18,7 @@ const DIST_DIR = './dist';
 
 app.use(express.static(DIST_DIR));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(DIST_DIR, 'index.html'));
 });
 
