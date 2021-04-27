799adfcaf6d21c1d33d6ce6eb750fa2456a9f447
Bryan Anderson <banderson5144@gmail.com>
2021-04-27 18:55:30 -0400

    fat arrow

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -60,7 +60,7 @@ export default class App extends LightningElement
         var HOST = location.origin.replace(/^http/, 'ws')
         var ws = new WebSocket(HOST);
 
-        ws.onmessage = function (event) {
+        ws.onmessage = (event) => {
             console.log(typeof(event));
             console.log(event);
             console.log(event.data);
