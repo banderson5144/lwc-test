54746d11371d98cd4ad6d40302488ecef95a6603
Bryan Anderson <banderson5144@gmail.com>
2021-04-27 19:34:25 -0400

    solve disconnecting

--- src/client/resources/somescript.js
+++ src/client/resources/somescript.js
@@ -1,8 +1,8 @@
-// var HOST = location.origin.replace(/^http/, 'ws')
-// var ws = new WebSocket(HOST);
+var HOST = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(HOST);
 
-// ws.onmessage = function (event) {
-//     console.log(typeof(event));
-//     console.log(event);
-//     console.log(event.data);
-// };
+ws.onmessage = function (event) {
+    console.log(typeof(event));
+    console.log(event);
+    console.log(event.data);
+};
